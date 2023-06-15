4372fc03649389ee7465d5073ae077b7ffffd02b
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-06-15 02:04:09 +0200

    push son

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Create from './components/create';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="main">
-      <h2 className="main-header">React Crud Operations</h2>
-    </div>
+    <Router>
+      <div className="main">
+        <h2 className="main-header">S Crud Operations</h2>
+        <div>
+          <Route exact path='/create' component={Create} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
